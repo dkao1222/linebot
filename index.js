c3e8e2ee9fd2155d612877ee503ba3e92cde549a
dkao1222 <dkao1222@gmail.com>
2019-11-21 15:04:29 +0800

    update request

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ bot.on('message', function (event) {
     var msg = event.message.text.toLowerCase()
     switch (msg) {
       case 'help':
-        event.reply('如果有任何電腦硬體相關問題請洽 TSG')
-        event.reply('大南路，機場，南工蒼可分機直播 #595 洽 HelpDesk')
-        event.reply(_buttonReply2actonViewHelp('HELP', 'Function', 'SmartView', 'no function2'))
+        //event.reply('如果有任何電腦硬體相關問題請洽 TSG')
+        //event.reply('大南路，機場，南工蒼可分機直播 #595 洽 HelpDesk')
+        event.reply(_buttonReply2actonViewHelp('HELP for SmartView', '如果有任何電腦硬體相關問題請洽 TSG', 'SmartView', 'no function2'))
         
         console.log(msg)
         break;
