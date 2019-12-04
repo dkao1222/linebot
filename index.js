acc2a258f0e5a04ee43f61c69c0ac6ed3668e71a
dkao1222 <dkao1222@gmail.com>
2019-12-04 22:21:30 +0800

    update function get Google

--- index.js
+++ index.js
@@ -112,14 +112,14 @@ bot.on('message', function (event) {
               //users[myId].replies[myStep] = event.message.text;
               console.log('email error')
               console.log(myStep);
-              console.log(myStep -1)
             } else if (myStep == 1) {
               var accountArray = ['shipping', 'receiving', 'is', 'arpc', 'reportprocess'];
               if (accountArray.indexOf(event.message.text.toLowerCase()) > 1) {
                 sendMessage(event, myQuestions[2][myStep] + '\n' + myQuestions[0][myStep]);
                 //users[myId].replies[myStep] = event.message.text;
+                console.log(myStep);
                 console.log('account error');
-                myStep -1
+                //myStep -1
               }
               
               
@@ -129,15 +129,17 @@ bot.on('message', function (event) {
               if (accountArray.indexOf(event.message.text.toLowerCase()) > 1) {
                 sendMessage(event, myQuestions[2][myStep] + '\n' + myQuestions[0][myStep]);
                 //users[myId].replies[myStep] = event.message.text;
+                console.log(myStep);
                 console.log('account error');
-                myStep -1
+                //myStep -1
               }
 
             } else if (myStep == 3 && event.message.text.length <= 20) {
               sendMessage(event, myQuestions[2][myStep] + '\n' + myQuestions[0][myStep]);
               //users[myId].replies[myStep] = event.message.text;
+              console.log(myStep);
               console.log('description length error');
-              myStep -1
+              //myStep -1
             }
             else {
               sendMessage(event, myQuestions[1][myStep] + '\n' + myQuestions[0][myStep + 1]);
