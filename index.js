990b2016fed9fd2a0bf67b94410416cac63384ee
Dragon Kao <dkao1222@gmail.com>
2021-05-03 16:16:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function handleEvent(event) {
   switch(event.message.text.toLowerCase()) 
   {
 	  case 'help':
-		messagebody  = 'how can i help you!, id:' + userId ', Name:' + userName
+		messagebody  = 'how can i help you!, id:' + userId + ', Name:' + userName
 		break;
 	  case 'submit':
 		messagebody  = 'what do you want submit case'
