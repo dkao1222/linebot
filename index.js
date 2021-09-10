be04a3a7d0e02ea278a9a97991f0520488776326
dkao1222 <dkao1222@gmail.com>
2021-09-10 16:24:51 +0800

    modify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-const handleEvent = require('./linehandleEvent.js')
+
 
 require('dotenv').config()
 
@@ -22,7 +22,7 @@ const app = express();
 
 app.post('/callback', line.middleware(config), (req, res) => {
     Promise
-        .all(req.body.events.map(handleEvent.handleEvent))
+        .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
         .catch((err) => {
             console.error(err);
@@ -30,7 +30,18 @@ app.post('/callback', line.middleware(config), (req, res) => {
         });
 });
 
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
 
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
 
 
 
