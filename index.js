939dbe32ef36c1b5bb436f308b4760bc460b4b78
dkao1222 <dkao1222@gmail.com>
2021-09-10 16:01:03 +0800

    feat:change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-const handleEvent = require('./linehandleEvent.js')
+//const handleEvent = require('./linehandleEvent.js')
 
 require('dotenv').config()
 
@@ -28,7 +28,67 @@ app.post('/callback', line.middleware(config), (req, res) => {
         });
 });
 
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+    var messagebody = ''
+
+
+    console.log(`使用者 ID: ${event.source.userId}`);
+
+    client.getProfile(event.source.userId)
+        .then((profile) => {
+            console.log(profile.userId);
+            console.log(profile.displayName);
+            console.log(profile.pictureUrl);
+            console.log(profile.statusMessage);
+
+
+        })
+        .catch((err) => {
+            // error handling
+        });
+
+
+
+    //var userId = event.source.userId
+    //var userName = event.source.profile.displayName
+    client.getProfile(event.source.userId)
+        .then((profile) => {
+            var currectUserName = ""
+            currectUserName = profile.displayName
+
+
+
+            switch (event.message.text.toLowerCase()) {
+                case 'help':
+                    messagebody = 'Hi, ' + currectUserName + ', how can i help you!'
+                    //messagebody  = 'how can i help you! id:' + client.getProfile(event.source.userId).then((profile)=>{ profile.displayName})
+                    break;
+                case 'submit':
+                    messagebody = 'what do you want submit case'
+
+                    break;
+                default:
+                    messagebody = event.message.text
+
+
+            }
+
+            const echo = { type: 'text', text: 'only echo ' + messagebody };
+            return client.replyMessage(event.replyToken, echo);
+
+        });
+
+}
+
+
+
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+
+
