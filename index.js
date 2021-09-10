76c9a90d6256713fdf08eddf0fbb85e3bb77bbcb
dkao1222 <dkao1222@gmail.com>
2021-09-10 16:15:25 +0800

    change function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-//const handleEvent = require('./linehandleEvent.js')
+const handleEvent = require('./linehandleEvent.js')
 
 require('dotenv').config()
 
@@ -30,18 +30,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
         });
 });
 
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        // ignore non-text-message event
-        return Promise.resolve(null);
-    }
 
-    // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
-
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
-}
 
 
 
--- linehandleEvent.js
+++ linehandleEvent.js
@@ -3,54 +3,10 @@ function handleEvent(event) {
         // ignore non-text-message event
         return Promise.resolve(null);
     }
-    var messagebody = ''
 
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
 
-    console.log(`使用者 ID: ${event.source.userId}`);
-
-    client.getProfile(event.source.userId)
-        .then((profile) => {
-            console.log(profile.userId);
-            console.log(profile.displayName);
-            console.log(profile.pictureUrl);
-            console.log(profile.statusMessage);
-
-
-        })
-        .catch((err) => {
-            // error handling
-        });
-
-
-
-    //var userId = event.source.userId
-    //var userName = event.source.profile.displayName
-    client.getProfile(event.source.userId)
-        .then((profile) => {
-            var currectUserName = ""
-            currectUserName = profile.displayName
-
-
-
-            switch (event.message.text.toLowerCase()) {
-                case 'help':
-                    messagebody = 'Hi, ' + currectUserName + ', how can i help you!'
-                    //messagebody  = 'how can i help you! id:' + client.getProfile(event.source.userId).then((profile)=>{ profile.displayName})
-                    break;
-                case 'submit':
-                    messagebody = 'what do you want submit case'
-
-                    break;
-                default:
-                    messagebody = event.message.text
-
-
-            }
-
-            const echo = { type: 'text', text: 'only echo ' + messagebody };
-            return client.replyMessage(event.replyToken, echo);
-
-        });
-
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
 }
-
