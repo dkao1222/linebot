dc99d91c2b03fb33fec4fa5c1e701de4a5443a54
dkao1222 <dkao1222@gmail.com>
2021-11-13 00:07:02 +0800

    c

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function handleEvent(event) {
     //var userData = []
     client.getProfile(event.source.userId)
         .then((profile) => {
-            //console.log(profile.userId);
+            console.log(profile.userId);
             //console.log(profile.displayName);
             //console.log(profile.pictureUrl);
             //console.log(profile.statusMessage);
@@ -126,7 +126,7 @@ async function userCheck(values) {
         var msgText = ""
         //console.log(resp)
         for (let index = 0; index <= resp.length; index++) {
-            if( resp[index][1] = values ) {
+            if( resp[index][1] == values ) {
                 console.log(resp[index][6])
                 msgText = resp[index][6]
                 break
